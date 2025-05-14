8a76d83e131e70e1bbaff7bf148ef4e126bd55b1
Matt Wheeler <50684499+mehteh@users.noreply.github.com>
2025-05-14 07:43:53 -0400

    Update install-brew.sh

--- setup-scripts/install-brew.sh
+++ setup-scripts/install-brew.sh
@@ -6,11 +6,14 @@ if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "Homebrew is already installed."
+    exit 0
 fi
 
 # Verify installation
 if command -v brew &> /dev/null; then
     echo "Homebrew installed successfully!"
+    exit 0
 else
     echo "Something went wrong. Please check the installation logs."
+    exit 1
 fi
