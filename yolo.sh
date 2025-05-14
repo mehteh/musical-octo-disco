8a4fd0a4e944e5b06666b58c296f59a65703b5f5
Matt Wheeler <50684499+mehteh@users.noreply.github.com>
2025-05-14 07:40:48 -0400

    Update yolo.sh

--- yolo.sh
+++ yolo.sh
@@ -4,7 +4,7 @@
 SCRIPT_NAME="Mac Software Installer and Customizer"
 VERSION="1.0"
 AUTHOR="Yeti"
-BASH_SCRIPT="https://raw.githubusercontent.com/mehteh/musical-octo-disco/main/setup-scripts/install-brew.sh"
+BREW_SCRIPT="https://raw.githubusercontent.com/mehteh/musical-octo-disco/main/setup-scripts/install-brew.sh"
 
 
 echo "===================================="
@@ -12,7 +12,7 @@ echo "  $SCRIPT_NAME"
 echo "  Version: $VERSION by $AUTHOR"
 echo "===================================="
 
-wget -qO- $BASH_SCRIPT | bash
+wget -qO- $BREW_SCRIPT | bash
 
 
 
