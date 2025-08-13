15fbd03296fbcd4de8ea147e9f7eb2fd26ecc53e
mehteh <yeti@techyeti.net>
2025-08-13 15:25:00 -0500

    Update Brewfile

--- setup-scripts/dump-brew.sh
+++ setup-scripts/dump-brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew bundle dump --force --file=./Brewfile
+brew bundle dump --force --file=./Brewfile --describe 
 git add Brewfile
 git -C ../ commit -am "Update Brewfile"
 git  push --force
